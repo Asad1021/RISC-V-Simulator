224dd3808d4497ca8bb82b397d3a3af814a03b56
Shiva-Koshta <114857995+Shiva-Koshta@users.noreply.github.com>
2023-03-08 00:43:40 +0530

    identified the type in decode
    1. Identify the type of inst.
    2. added decode class

--- prog.cpp
+++ prog.cpp
@@ -9,14 +9,23 @@ using namespace std;
     // stores the file name of the dump file
     string filename;
     ifstream readFile("input.mc");
-    
 #pragma endregion FILE_RELATED_DATA 
+
 
 #pragma region INSTRUCTION_RELATED_DATA 
     int currentPCAdd = 0;
     bitset<32> currentInstruction;
 #pragma endregion INSTRUCTION_RELATED_DATA 
 
+
+#pragma region DECODE_RELATED_DATA
+typedef struct if_de_handshake{
+    
+
+} IfDE;
+
+IfDE hs1;
+#pragma endregion DECODE_RELATED_DATA
 
 class Fetch
 {
@@ -47,7 +56,8 @@ class Fetch
 void make_file()
 {
         cout << "Enter input filename: ";
-        cin >> filename;
+        // cin >> filename;
+        filename = "input.txt";
 
 
         ifstream infile;
@@ -68,13 +78,13 @@ void make_file()
         
         while (getline(infile, line))
         {
-            cout << "0x" << offset << " " << line << endl; 
+            // cout << "0x" << offset << " " << line << endl; 
             //skip 0x part
             string hex_str = line;
             //stoul converts string of type 0x012312 to its decimal value
             unsigned long hex_to_dec_val = stoul(hex_str, nullptr, 16);
             bitset<32> binary_num(hex_to_dec_val);
-            cout<<binary_num<<endl;
+            // cout<<binary_num<<endl;
             outfile << "0x" << offset << " " << line << endl;
             offset += 4; // increase offset by 4 characters
         }
@@ -82,6 +92,100 @@ void make_file()
         infile.close();
         outfile.close(); 
 }
+
+class Decode
+{
+    void Decode_Instruction()
+    {
+        bitset<5> opcode;
+        char type_of_instruction;
+
+        for (int i = 0; i < 5; i++)//ignoring last two bit as these are always 11
+        {
+            opcode[i]=currentInstruction[i+2];
+        }
+        // cout<<opcode;
+
+        if((!opcode[4])&&opcode[3]&&opcode[2]&&(!opcode[1])&&(!opcode[0]))//01100
+        type_of_instruction = 'R';
+
+        else if((!opcode[4])&&(!opcode[3])&&(!opcode[1])&&(!opcode[0]))//00X00
+        type_of_instruction = 'I';
+
+        else if((!opcode[4])&&opcode[3]&&(!opcode[2])&&(!opcode[1])&&(!opcode[0]))//01000
+        type_of_instruction = 'S';
+
+        else if(opcode[4]&&opcode[3]&&(!opcode[2])&&(!opcode[1])&&(!opcode[0]))//11000
+        type_of_instruction = 'B';
+
+        else if(opcode[4]&&opcode[3]&&(!opcode[2])&&opcode[1]&&opcode[0])//11011
+        type_of_instruction = 'J';
+
+        else if(opcode[4]&&opcode[3]&&(!opcode[2])&&(!opcode[1])&&opcode[0])//11001
+        type_of_instruction = 'I';
+
+        else if((!opcode[4])&&opcode[2]&&(!opcode[1])&&opcode[0])//0X101
+        type_of_instruction = 'U';
+
+        else if(opcode[4]&&opcode[3]&&opcode[2]&&(!opcode[1])&&(!opcode[0]))//11100
+        type_of_instruction = 'I';
+
+        else type_of_instruction = 'Z';
+
+        // cout<<type_of_instruction;
+
+        switch (type_of_instruction)
+        {
+            case 'R':
+               {
+                
+               }
+                break;
+
+            case 'I':
+                {
+                    
+                }
+                break;
+    
+            case 'B':
+               {
+                
+               }
+                break;
+            
+            case 'U':
+               {
+                
+               }
+                break;
+            
+            case 'J':
+                {
+                    
+                }
+                break;
+
+            
+            default: 
+                {
+                    cerr << "Error: Could not identify instruction.\n";
+                    exit(0);
+                }
+                break;
+        }
+        
+
+    }
+
+    public:
+
+    Decode()
+    {
+        Decode_Instruction();
+    }
+
+};
 
 void reset_pointer()
 {
@@ -91,8 +195,13 @@ int main()
 {
     make_file();
     //whenever you create an object pc increments by 4 and new instruction is loaded in currentInstruction global var
+    // Fetch a;
+    // Fetch b;
+    // cout<<endl<<"Current instr"<<currentInstruction;
+
     Fetch a;
-    Fetch b;
-    cout<<endl<<"Current instr"<<currentInstruction;
+    cout<<endl;
+    Decode A;
+
     return 0;
 }
