a223b40bc7a65bc7d8313e11244d4b7c59773c79
Sarvottam <sarvottamswaroop100@gmail.com>
2023-03-09 12:14:51 +0530

    Now fetch object takes flag as input, by default 0 if 1 then next instruction is taken from next address global variable ie nextPCAdd

--- prog.cpp
+++ prog.cpp
@@ -13,34 +13,72 @@ using namespace std;
 #pragma endregion FILE_RELATED_DATA 
 
 #pragma region INSTRUCTION_RELATED_DATA 
-    int currentPCAdd = 0;
+    bitset<32> currentPCAdd(0);
+    bitset<32> nextPCAdd(0);
     bitset<32> currentInstruction;
 #pragma endregion INSTRUCTION_RELATED_DATA 
 
 
 class Fetch
 {
-    bitset<32> fetch_instruction()
+    bitset<32> fetch_instruction(int flag)
     {
-        string line;
-        //to ignore the first part before space
-        readFile>>line;
-        readFile>>line;
-        string hex_str = line;
+        //the part b4 space PC address
+        string hex_str;
+        
+        if(flag == 0)
+        {
+            string pc_str = read();
+            currentPCAdd = HexStringToBitset(pc_str);
+            hex_str = read();
+            addToBitset();
+        }
+        else if(flag == 1)
+        {
+            currentPCAdd = nextPCAdd;
+            ifstream tempRead("input.mc");
+            string tempHexa;
+            tempRead>>tempHexa;
+            bitset<32> tempBitset(HexStringToBitset(tempHexa));
+            while(tempBitset != nextPCAdd)
+            {
+                tempRead>>tempHexa;
+                tempRead>>tempHexa;
+                tempBitset = HexStringToBitset(tempHexa); 
+            }
+
+            tempRead>>hex_str;
+        }
         //stoul converts string of type 0x012312 to its decimal value
-        unsigned long hex_to_dec_val = stoul(hex_str, nullptr, 16);
-        bitset<32> binary_num(hex_to_dec_val);
-        cout<<binary_num;
-        return binary_num;
+        return HexStringToBitset(hex_str);
 
     }
-    public:
-    Fetch()
+    string read()
     {
-        //everytime object created increase the value of PC Address by 4
-        currentPCAdd += 4;
-        currentInstruction = fetch_instruction();
+        string line;
+        readFile>>line;
+        return line;
     }
+    void addToBitset()
+    {
+        currentPCAdd = currentPCAdd.to_ulong() + 4; // add 4 to the bitset
+        
+    }
+
+    bitset<32> HexStringToBitset(string hex_instr)
+    {
+        unsigned long hex_to_dec_val = stoul(hex_instr, nullptr, 16);
+        bitset<32> binary_num(hex_to_dec_val);
+        return binary_num;
+    }
+
+    public:
+    Fetch(int flag = 0)
+    {
+       
+        currentInstruction = fetch_instruction(flag);
+    }
+    
 
 };
 //makes .mc file
@@ -68,14 +106,14 @@ void make_file()
         
         while (getline(infile, line))
         {
-            cout << "0x" << offset << " " << line << endl; 
+            cout << "0x" << hex<<offset << " " << line << endl; 
             //skip 0x part
             string hex_str = line;
             //stoul converts string of type 0x012312 to its decimal value
             unsigned long hex_to_dec_val = stoul(hex_str, nullptr, 16);
             bitset<32> binary_num(hex_to_dec_val);
             cout<<binary_num<<endl;
-            outfile << "0x" << offset << " " << line << endl;
+            outfile << "0x" <<hex<<offset << " " << line << endl;
             offset += 4; // increase offset by 4 characters
         }
 
@@ -93,6 +131,10 @@ int main()
     //whenever you create an object pc increments by 4 and new instruction is loaded in currentInstruction global var
     Fetch a;
     Fetch b;
-    cout<<endl<<"Current instr"<<currentInstruction;
+    // cout<<endl<<"Current instr "<<currentInstruction<<endl;
+    bitset<32> nextadd(8);
+    nextPCAdd = nextadd;
+    Fetch c(1);
+    cout<<endl<<"current pc add"<<currentPCAdd<<"Current instr "<<currentInstruction<<endl;
     return 0;
 }
