90d35b01d8e2dad22d2646d57146f5ebd6695be4
Shiva Koshta <114857995+Shiva-Koshta@users.noreply.github.com>
2023-03-13 15:39:18 +0530

    updated merge sort program

--- prog.cpp
+++ prog.cpp
@@ -4,7 +4,7 @@
 #include <bitset>
 #include <string>
 
-#define MEMORY_SIZE 1200001
+#define MEMORY_SIZE 120000
 //immb conversion is always a unsigned operation hence we have to convert it to signed one by using typecast
 //immB+PC; immB=immb.to_ulong();//unsigned
 
@@ -1046,7 +1046,6 @@ class Write_Back{
 void RISCv_Processor()
 {
     RF[2]=MEMORY_SIZE-1;
-    string buff;
     while(1)
     {
         Fetch a(1);
@@ -1054,8 +1053,6 @@ void RISCv_Processor()
         Execute c;
         Memory_Acess d;
         Write_Back e;
-        // cin>>buff;
-
     }
 }
 
